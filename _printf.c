309ba8b7f1d54c36b5262bd53f1807cf4cbb56ca
AbelTB <abeltefera.com>
2022-07-12 07:52:32 -0700

    Task 0

--- _printf.c
+++ _printf.c
@@ -1,43 +1,50 @@
 #include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+
 /**
- * _printf - printf function
- * @format: const char pointer
- * Return: b_len
+ * _printf - function that produces output according to a format
+ * @format: type of argument passed to function
+ *
+ * Return: k, number of characters printed
  */
+
 int _printf(const char *format, ...)
 {
-	int (*pfunc)(va_list, flags_t *);
-	const char *p;
-	va_list arguments;
-	flags_t flags = {0, 0, 0};
+	va_list args;
+	int i, j, k, count;
+	var_t type[] = {
+		{"c", c_func}, {"s", s_func}, {"i", i_func}, {"%", perc_func},
+		{"d", d_func},	{"b", b_func},	{"r", rev_func}, {"R", rot_func},
+		{NULL, NULL},
+	};
 
-	register int count = 0;
-
-	va_start(arguments, format);
-	if (!format || (format[0] == '%' && !format[1]))
+	va_start(args, format);
+	i = 0, count = 0, k = 0;
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = format; *p; p++)
+	while (format && format[i])
 	{
-		if (*p == '%')
+		if (format[i] != '%')
+			_putchar(format[i]), k++;
+		else
 		{
-			p++;
-			if (*p == '%')
+			j = 0;
+			while (type[j].vartype)
 			{
-				count += _putchar('%');
-				continue;
+				if (format[i + 1] == *type[j].vartype)
+				{
+					count += (type[j].f)(args), i++;
+					break;
+				}
+				j++;
 			}
-			while (get_flag(*p, &flags))
-				p++;
-			pfunc = get_print(*p);
-			count += (pfunc)
-				? pfunc(arguments, &flags)
-				: _printf("%%%c", *p);
-		} else
-			count += _putchar(*p);
+			if (type[j].vartype == NULL)
+				count += 1, _putchar('%');
+		}
+		i++;
 	}
-	_putchar(-1);
-	va_end(arguments);
-	return (count);
+	k += count;
+	va_end(args);
+	return (k);
 }
